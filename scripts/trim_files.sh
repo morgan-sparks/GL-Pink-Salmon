44c87e8d3d76213027c30e415952d6e16639fc26
morgan-sparks <msparks1309@gmail.com>
2021-09-03 14:46:41 -0400

    fix trim_files.sh

--- scripts/trim_files.sh
+++ scripts/trim_files.sh
@@ -23,37 +23,36 @@ FASTQC_OUT=$OUTPUT/trimmed_fastQC/
 
 while read -a line
 do
-trimmomatic PE -phred33 -threads 20\ #### run 1 reads ###
-$RAWDATA/${line[0]}*R1*run1.fastq.gz \
-$RAWDATA/${line[0]}*R2*run1.fastq.gz \
-$OUPUT/trimmed_paired_${line[0]}_R1_run1.fastq.qz \
-$OUPUT/trimmed_unpaired_${line[0]}_R1_run1.fastq.gz \
-$OUPUT/trimmed_paired_${line[0]}_R2_run1.fastq.gz \
-$OUPUT/trimmed_unpaired_${line[0]}_R2_run1.fastq.gz \
-LEADING:20 TRAILING:20 MINLEN:30 \
-ILLUMINACLIP:/scratch/bell/sparks35/GL_Pink_Salmon/data/IUPUI_adapter.fa:2:40:10
-
-trimmomatic PE -phred33 -threads 20 \ #### run 2 reads ###
-$RAWDATA/${line[0]}*R1*run2.fastq.gz \
-$RAWDATA/${line[0]}*R2*run2.fastq.gz \
-$OUPUT/trimmed_paired_${line[0]}_R1_run2.fastq.qz \
-$OUPUT/trimmed_unpaired_${line[0]}_R1_run2.fastq.gz \
-$OUPUT/trimmed_paired_${line[0]}_R2_run2.fastq.gz \
-$OUPUT/trimmed_unpaired_${line[0]}_R2_run2.fastq.gz \
-LEADING:20 TRAILING:20 MINLEN:30 \
-ILLUMINACLIP:/scratch/bell/sparks35/GL_Pink_Salmon/data/IUPUI_adapter.fa:2:40:10
-
-fastqc -t 10 -o $FASTQC_OUT \
-trimmed_paired_${line[0]}_R1_run1.fastq.qz \
-trimmed_unpaired_${line[0]}_R1_run1.fastq.gz \
-trimmed_paired_${line[0]}_R2_run1.fastq.gz \
-trimmed_unpaired_${line[0]}_R2_run1.fastq.gz \
-
-fastqc -t 10 -o $FASTQC_OUT \
-trimmed_paired_${line[0]}_R1_run2.fastq.qz \
-trimmed_unpaired_${line[0]}_R1_run2.fastq.gz \
-trimmed_paired_${line[0]}_R2_run2.fastq.gz \
-trimmed_unpaired_${line[0]}_R2_run2.fastq.gz \
+  trimmomatic PE -phred33 -threads 20\ #### run 1 reads ###
+  $RAWDATA/${line[0]}*R1*run1.fastq.gz \
+  $RAWDATA/${line[0]}*R2*run1.fastq.gz \
+  $OUPUT/trimmed_paired_${line[0]}_R1_run1.fastq.qz \
+  $OUPUT/trimmed_unpaired_${line[0]}_R1_run1.fastq.gz \
+  $OUPUT/trimmed_paired_${line[0]}_R2_run1.fastq.gz \
+  $OUPUT/trimmed_unpaired_${line[0]}_R2_run1.fastq.gz \
+  LEADING:20 TRAILING:20 MINLEN:30 \
+  ILLUMINACLIP:/scratch/bell/sparks35/GL_Pink_Salmon/data/IUPUI_adapter.fa:2:40:10
+  
+  trimmomatic PE -phred33 -threads 20 \ #### run 2 reads ###
+  $RAWDATA/${line[0]}*R1*run2.fastq.gz \
+  $RAWDATA/${line[0]}*R2*run2.fastq.gz \
+  $OUPUT/trimmed_paired_${line[0]}_R1_run2.fastq.qz \
+  $OUPUT/trimmed_unpaired_${line[0]}_R1_run2.fastq.gz \
+  $OUPUT/trimmed_paired_${line[0]}_R2_run2.fastq.gz \
+  $OUPUT/trimmed_unpaired_${line[0]}_R2_run2.fastq.gz \
+  LEADING:20 TRAILING:20 MINLEN:30 \
+  ILLUMINACLIP:/scratch/bell/sparks35/GL_Pink_Salmon/data/IUPUI_adapter.fa:2:40:10
+  
+  fastqc -t 10 -o $FASTQC_OUT \
+  trimmed_paired_${line[0]}_R1_run1.fastq.qz \
+  trimmed_unpaired_${line[0]}_R1_run1.fastq.gz \
+  trimmed_paired_${line[0]}_R2_run1.fastq.gz \
+  trimmed_unpaired_${line[0]}_R2_run1.fastq.gz 
+  
+  fastqc -t 10 -o $FASTQC_OUT trimmed_paired_${line[0]}_R1_run2.fastq.qz \
+  trimmed_unpaired_${line[0]}_R1_run2.fastq.gz \
+  trimmed_paired_${line[0]}_R2_run2.fastq.gz \
+  trimmed_unpaired_${line[0]}_R2_run2.fastq.gz 
 done < $PROJHOME/data/sample_names.txt
 
 multiqc $FASTQC_OUT

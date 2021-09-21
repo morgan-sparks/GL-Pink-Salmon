439d70a8d66019a53977565e7d59341fbf4bb499
morgan-sparks <msparks1309@gmail.com>
2021-09-20 23:02:35 -0400

    fix spacing issue

--- scripts/align_reads.sh
+++ scripts/align_reads.sh
@@ -20,15 +20,16 @@ PROJHOME=/scratch/bell/sparks35/GL_Pink_Salmon
 
 while read -a line
 do
-  bwa mem -t 20 -M $PROJHOME/data/assemblies/Ogor_1.0/GCA_017355495.1_Ogor_1.0_genomic.fna \
-  $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R1_run1.fastq \ 
-  $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R2_run1.fastq \
-  > $PROJHOME/data/seqs/aligned_reads_Ogor1.0/${line[0]}_run1_Ogor1.0_aligned.bam
+
+bwa mem -t 20 -M $PROJHOME/data/assemblies/Ogor_1.0/GCA_017355495.1_Ogor_1.0_genomic.fna \
+$PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R1_run1.fastq \
+$PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R2_run1.fastq \
+> $PROJHOME/data/seqs/aligned_reads_Ogor1.0/${line[0]}_run1_Ogor1.0_aligned.bam
   
-  bwa mem -t 20 -M $PROJHOME/data/assemblies/Ogor_1.0/GCA_017355495.1_Ogor_1.0_genomic.fna \
-  $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R1_run2.fastq \ 
-  $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R2_run2.fastq \
-  > $PROJHOME/data/seqs/aligned_reads_Ogor1.0/${line[0]}_run2_Ogor1.0_aligned.bam
+bwa mem -t 20 -M $PROJHOME/data/assemblies/Ogor_1.0/GCA_017355495.1_Ogor_1.0_genomic.fna \
+$PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R1_run2.fastq \
+$PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_${line[0]}_R2_run2.fastq \
+> $PROJHOME/data/seqs/aligned_reads_Ogor1.0/${line[0]}_run2_Ogor1.0_aligned.bam
 
 done < $PROJHOME/data/sample_names.txt
 

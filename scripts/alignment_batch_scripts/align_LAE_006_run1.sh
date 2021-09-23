55c9befdcf200aa5f7cfcdcfa561e7fc9ef267a7
morgan-sparks <msparks1309@gmail.com>
2021-09-23 14:12:52 -0400

    change to 1 node, 126 cores

--- scripts/alignment_batch_scripts/align_LAE_006_run1.sh
+++ scripts/alignment_batch_scripts/align_LAE_006_run1.sh
@@ -3,7 +3,8 @@
 #SBATCH --job-name=align_LAE_006_run1
 #SBATCH -A standby
 #SBATCH -t 4:00:00
-#SBATCH -N 10
+#SBATCH -N 1
+#SBATCH -n 126
 
 module purge
 module load bioinfo
@@ -13,4 +14,4 @@ PROJHOME=/scratch/bell/sparks35/GL_Pink_Salmon
 
 cd $PROJHOME/data/seqs/aligned_reads_Ogor1.0/
 
-bwa mem -t 10 -M $PROJHOME/data/assemblies/Ogor_1.0/GCA_017355495.1_Ogor_1.0_genomic.fna $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_LAE_006_R1_run1.fastq $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_LAE_006_R2_run1.fastq > $PROJHOME/data/seqs/aligned_reads_Ogor1.0/batch_output/LAE_006_run1_Ogor1.0_aligned.sam
+bwa mem -t 126 -M $PROJHOME/data/assemblies/Ogor_1.0/GCA_017355495.1_Ogor_1.0_genomic.fna $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_LAE_006_R1_run1.fastq $PROJHOME/data/seqs/trimmed_seqs/trimmed_paired_LAE_006_R2_run1.fastq > $PROJHOME/data/seqs/aligned_reads_Ogor1.0/batch_output/LAE_006_run1_Ogor1.0_aligned.sam

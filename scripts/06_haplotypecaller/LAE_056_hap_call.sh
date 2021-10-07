7c537961841ba6acb7a3e8e891158736ca38bca6
morgan-sparks <msparks1309@gmail.com>
2021-10-06 22:25:22 -0400

    fix parallelize

--- scripts/06_haplotypecaller/LAE_056_hap_call.sh
+++ scripts/06_haplotypecaller/LAE_056_hap_call.sh
@@ -14,12 +14,12 @@ ASSEMBLY=/scratch/bell/sparks35/GL_Pink_Salmon/data/assemblies/Ogor_1.0/GCA_0173
 MDUPES=/scratch/bell/sparks35/GL_Pink_Salmon/data/seqs/aligned_reads_Ogor1.0/mark_dupes
 HAPCALLS=/scratch/bell/sparks35/GL_Pink_Salmon/data/seqs/aligned_reads_Ogor1.0/06_hap_calls
 
-while read -a line
+cat $PROJHOME/data/seqs/aligned_reads_Ogor1.0/chromosome_list.txt | while read -a line
 do
 /home/sparks35/gatk-4.2.2.0/gatk --java-options "-Xmx9g -Djava.io.tmpdir=/scratch/bell/sparks35/tmpdir" HaplotypeCaller \
 -I $MDUPES/LAE_056_Ogor1.0_dupmarked.bam \
 -O $HAPCALLS/LAE_056_Ogor1.0.vcf.gz \
 -R $ASSEMBLY \
 -ERC GVCF \
--L ${line[0]} &
-done < $PROJHOME/data/seqs/aligned_reads_Ogor1.0/chromosome_list.txt 
+-L ${line[0]}
+& done 

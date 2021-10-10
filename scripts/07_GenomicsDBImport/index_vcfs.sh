94c31f11057528d0a19ddba81732c175846f5a72
morgan-sparks <msparks1309@gmail.com>
2021-10-10 12:16:46 -0400

    index VCFs

---
+++ scripts/07_GenomicsDBImport/index_vcfs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=index_VCFs
+#SBATCH -A standby
+#SBATCH -N 1
+#SBATCH -n 126
+#SBATCH -t 4:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=sparks35@purdue.edu
+
+module purgue
+module load bioinfo
+modul load bcftools/1.11
+
+PROJHOME=/scratch/bell/sparks35/GL_Pink_Salmon
+
+while read -a line
+do
+bcftools $PROJHOME/data/seqs/aligned_reads_Ogor1.0/06_hap_calls/${line[0]}
+done <$PROJHOME/data/sample_names.txt

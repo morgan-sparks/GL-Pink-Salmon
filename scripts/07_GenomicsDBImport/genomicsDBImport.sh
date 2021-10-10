9ed2aeac43f659d65f3490ca8b1d0347b9b235dd
morgan-sparks <msparks1309@gmail.com>
2021-10-10 10:44:52 -0400

    add GenomicsDBImport shell script

---
+++ scripts/07_GenomicsDBImport/genomicsDBImport.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=genomicsDBImport
+#SBATCH -A highmem
+#SBATCH -N 1
+#SBATCH -n 126
+#SBATCH -t 24:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=sparks35@purdue.edu
+
+module purge
+PROJHOME=/scratch/bell/sparks35/GL_Pink_Salmon
+
+while read -a line
+do
+/home/sparks35/gatk-4.2.2.0/gatk --java-options "-Xmx36g -Xms36g" GenomicsDBImport \
+--$PROJHOME/data/seqs/aligned_reads_Ogor1.0/07_genomicsDB my_database \
+--batch-size 50 \
+-L ${line[0]} \
+--sample-name-map $PROJHOME/scripts/07_GenomicsDBImport/genomicsDB.tsv \
+--tmp-dir=scratch/bell/sparks35/tmpdir \
+--reader-threads 4 &
+done < $PROJHOME/data/seqs/aligned_reads_Ogor1.0/chromosome_list.txt
+wait

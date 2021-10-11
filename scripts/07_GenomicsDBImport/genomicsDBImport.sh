51d2f68074225ad68f9a8869f89ed8de14822d73
morgan-sparks <msparks1309@gmail.com>
2021-10-10 20:12:03 -0400

    try stacktrace

--- scripts/07_GenomicsDBImport/genomicsDBImport.sh
+++ scripts/07_GenomicsDBImport/genomicsDBImport.sh
@@ -12,7 +12,7 @@ PROJHOME=/scratch/bell/sparks35/GL_Pink_Salmon
 
 while read -a line
 do
-/home/sparks35/gatk-4.2.2.0/gatk --java-options "-Xmx36g -Xms36g" GenomicsDBImport \
+/home/sparks35/gatk-4.2.2.0/gatk --java-options "-Xmx36g -Xms36g -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" GenomicsDBImport \
 --genomicsdb-workspace-path $PROJHOME/data/seqs/aligned_reads_Ogor1.0/07_genomicsDB/${line[0]}_database \
 --batch-size 50 \
 -L ${line[0]} \

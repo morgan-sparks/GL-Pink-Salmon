cae431c34bf3897f39abec11fd180f45f1be8eef
morgan-sparks <msparks1309@gmail.com>
2021-10-12 14:20:44 -0400

    make file extensions g.vcf.gz

--- scripts/06_haplotypecaller/hapcall_LAE_088.sh
+++ scripts/06_haplotypecaller/hapcall_LAE_088.sh
@@ -23,10 +23,23 @@ while read -a line
 do
 /home/sparks35/gatk-4.2.2.0/gatk --java-options "-Xmx9g -Djava.io.tmpdir=/scratch/bell/sparks35/tmpdir" HaplotypeCaller \
 -I $MDUPES/LAE_088_Ogor1.0_dupmarked.bam \
--O $HAPCALLS/${line[0]}_LAE_088_Ogor1.0.vcf.gz \
+-O $HAPCALLS/${line[0]}_LAE_088_Ogor1.0.g.vcf.gz \
 -R $ASSEMBLY \
 -ERC GVCF \
 -L ${line[0]} &
 done < $PROJHOME/data/seqs/aligned_reads_Ogor1.0/chromosome_list.txt
 wait
+
+cd /scratch/bell/sparks35/GL_Pink_Salmon/data/seqs/aligned_reads_Ogor1.0/06_hap_calls
+
+bcftools concat -Oz \
+CM029847.1_LAE_088_Ogor1.0.g.vcf.gz CM029861.1_LAE_088_Ogor1.0.g.vcf.gz CM029848.1_LAE_088_Ogor1.0.g.vcf.gz CM029862.1_LAE_088_Ogor1.0.g.vcf.gz \
+CM029849.1_LAE_088_Ogor1.0.g.vcf.gz CM029863.1_LAE_088_Ogor1.0.g.vcf.gz CM029850.1_LAE_088_Ogor1.0.g.vcf.gz CM029864.1_LAE_088_Ogor1.0.g.vcf.gz \
+CM029851.1_LAE_088_Ogor1.0.g.vcf.gz CM029865.1_LAE_088_Ogor1.0.g.vcf.gz CM029852.1_LAE_088_Ogor1.0.g.vcf.gz CM029866.1_LAE_088_Ogor1.0.g.vcf.gz \
+CM029853.1_LAE_088_Ogor1.0.g.vcf.gz CM029867.1_LAE_088_Ogor1.0.g.vcf.gz CM029854.1_LAE_088_Ogor1.0.g.vcf.gz CM029868.1_LAE_088_Ogor1.0.g.vcf.gz \
+CM029855.1_LAE_088_Ogor1.0.g.vcf.gz CM029869.1_LAE_088_Ogor1.0.g.vcf.gz CM029856.1_LAE_088_Ogor1.0.g.vcf.gz CM029870.1_LAE_088_Ogor1.0.g.vcf.gz \
+CM029857.1_LAE_088_Ogor1.0.g.vcf.gz CM029871.1_LAE_088_Ogor1.0.g.vcf.gz CM029858.1_LAE_088_Ogor1.0.g.vcf.gz CM029872.1_LAE_088_Ogor1.0.g.vcf.gz \
+CM029859.1_LAE_088_Ogor1.0.g.vcf.gz CM029873.1_LAE_088_Ogor1.0.g.vcf.gz CM029860.1_LAE_088_Ogor1.0.g.vcf.gz > LAE_088_Ogor1.0_hapcalls.g.vcf.gz
+
+tabix --csi LAE_088_Ogor1.0_hapcalls.g.vcf.gz
 
--- scripts/06_haplotypecaller/hapcall_STL_113.sh
+++ scripts/06_haplotypecaller/hapcall_STL_113.sh
@@ -23,10 +23,23 @@ while read -a line
 do
 /home/sparks35/gatk-4.2.2.0/gatk --java-options "-Xmx9g -Djava.io.tmpdir=/scratch/bell/sparks35/tmpdir" HaplotypeCaller \
 -I $MDUPES/STL_113_Ogor1.0_dupmarked.bam \
--O $HAPCALLS/${line[0]}_STL_113_Ogor1.0.vcf.gz \
+-O $HAPCALLS/${line[0]}_STL_113_Ogor1.0.g.vcf.gz \
 -R $ASSEMBLY \
 -ERC GVCF \
 -L ${line[0]} &
 done < $PROJHOME/data/seqs/aligned_reads_Ogor1.0/chromosome_list.txt
 wait
+
+cd /scratch/bell/sparks35/GL_Pink_Salmon/data/seqs/aligned_reads_Ogor1.0/06_hap_calls
+
+bcftools concat -Oz \
+CM029847.1_STL_113_Ogor1.0.g.vcf.gz CM029861.1_STL_113_Ogor1.0.g.vcf.gz CM029848.1_STL_113_Ogor1.0.g.vcf.gz CM029862.1_STL_113_Ogor1.0.g.vcf.gz \
+CM029849.1_STL_113_Ogor1.0.g.vcf.gz CM029863.1_STL_113_Ogor1.0.g.vcf.gz CM029850.1_STL_113_Ogor1.0.g.vcf.gz CM029864.1_STL_113_Ogor1.0.g.vcf.gz \
+CM029851.1_STL_113_Ogor1.0.g.vcf.gz CM029865.1_STL_113_Ogor1.0.g.vcf.gz CM029852.1_STL_113_Ogor1.0.g.vcf.gz CM029866.1_STL_113_Ogor1.0.g.vcf.gz \
+CM029853.1_STL_113_Ogor1.0.g.vcf.gz CM029867.1_STL_113_Ogor1.0.g.vcf.gz CM029854.1_STL_113_Ogor1.0.g.vcf.gz CM029868.1_STL_113_Ogor1.0.g.vcf.gz \
+CM029855.1_STL_113_Ogor1.0.g.vcf.gz CM029869.1_STL_113_Ogor1.0.g.vcf.gz CM029856.1_STL_113_Ogor1.0.g.vcf.gz CM029870.1_STL_113_Ogor1.0.g.vcf.gz \
+CM029857.1_STL_113_Ogor1.0.g.vcf.gz CM029871.1_STL_113_Ogor1.0.g.vcf.gz CM029858.1_STL_113_Ogor1.0.g.vcf.gz CM029872.1_STL_113_Ogor1.0.g.vcf.gz \
+CM029859.1_STL_113_Ogor1.0.g.vcf.gz CM029873.1_STL_113_Ogor1.0.g.vcf.gz CM029860.1_STL_113_Ogor1.0.g.vcf.gz > STL_113_Ogor1.0_hapcalls.g.vcf.gz
+
+tabix --csi STL_113_Ogor1.0_hapcalls.g.vcf.gz
 
